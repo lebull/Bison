dc7d8c71a7cb7845f4759d8e9446a178968286e9
lebull <bulldogboy912000@yahoo.com>
2017-12-03 11:28:05 -0600

    vjoy implimentation

--- receiver.py
+++ receiver.py
@@ -3,9 +3,20 @@ from tornado.iostream import StreamClosedError
 from tornado import gen
 import tornado.ioloop
 
+import pyvjoy
+
+
 PORT = 8990
 
+
+
+
 class EchoServer(TCPServer):
+
+    def __init__(self, *args, **kwargs):
+        self.vjoy = pyvjoy.VJoyDevice(1)
+        super(EchoServer, self).__init__(*args, **kwargs)
+
     @gen.coroutine
     def handle_stream(self, stream, address):
         while True:
@@ -13,6 +24,12 @@ class EchoServer(TCPServer):
                 data = yield stream.read_until(b"\n")
                 #yield stream.write(data)
                 print data
+
+                message = data.split(", ")
+                did = int(message[0])
+                state = int(message[1])
+                print "Setting {} to {}".format(did, state)
+                self.vjoy.set_button(did,state)
             except StreamClosedError:
                 break
 
