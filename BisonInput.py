a2f145db016efc4b1a5fff45b15c7de2021b40e5
lebull <bulldogboy912000@yahoo.com>
2017-12-04 19:28:04 -0600

    Implemented AnalogInput

--- BisonInput.py
+++ BisonInput.py
@@ -63,7 +63,32 @@ class DirectButton(Button):
     def tick(self):
         self.setPressed( GPIO.input(self.pin) )
 
+class AnalogInput(object):
+    def __init__(self, bus, device, channel, outer_deadzone = None):
 
+        self.bus = bus
+        self.device = device
+        self.channel = channel
+
+        self.odz = 0 #outer deadzone to account for values never reaching 0 or 1 exactly
+        if(outer_deadzone):
+            self.odz = outer_deadzone
+
+
+        self.spi = spidev.SpiDev()
+        self.spi.open(0, 0)
+
+
+    def read(self):
+        channel = self.channel
+        odz = self.odz
+
+        rawData = self.spi.xfer([1, (8 + channel) << 4, 0])
+        raw_percentage = round(((rawData[1] * 255 + rawData[2]) / 1024.0), 4)
+        adjusted_percentage = (1 + (odz * 2)) * raw_percentage - odz
+        clamped_percent = min(max(0, adjusted_percentage), 1)
+
+        return clamped_percent
 
 if __name__ == "__main__":
     pass
